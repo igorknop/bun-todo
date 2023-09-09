d20b9640f68b09abce05089eb17c013cc91d8e6b
Igor Knop <igorknop@gmail.com>
2023-09-09 17:09:44 -0300

    add react response

--- index.tsx
+++ index.tsx
@@ -1,17 +1,43 @@
+import { renderToString } from "react-dom/server";
+
 const server = Bun.serve({
-    hostname: 'localhost',
-    port: 3000,
-    fetch: handler,
+  hostname: "localhost",
+  port: 3000,
+  fetch: handler,
 });
 
 console.log(`Server running at http://${server.hostname}:${server.port}/`);
 
-
-function handler(request:Request):Response {
+function handler(request: Request): Response {
   const url = new URL(request.url);
-  if(url.pathname === '/' || url.pathname === '/index.html' || url.pathname === '') {
-    return new Response(Bun.file('./index.html'));
+  if (
+    url.pathname === "/" ||
+    url.pathname === "/index.html" ||
+    url.pathname === ""
+  ) {
+    return new Response(Bun.file("./index.html"));
+  }
+  if (url.pathname === "/todos" && request.method === "POST") {
+    return new Response(renderToString(<TodoList todos={[]} />));
+  }
+  if (url.pathname === "/todos" && request.method === "GET") {
+    return new Response(renderToString(<TodoList todos={[]} />));
   }
   return new Response(`Not Found: ${url}`, { status: 404 });
-  
+}
+
+function TodoList(props: { todos: { id: number; text: string }[] }) {
+  return (
+    <ul>
+      {props.todos.length ? (
+        props.todos.map((todo) => (
+          <li>
+            {todo.id}: {todo.text}
+          </li>
+        ))
+      ) : (
+        <li>Empty</li>
+      )}
+    </ul>
+  );
 }
