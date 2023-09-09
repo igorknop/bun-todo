9bf0a89d2328fc58272a37cda2735972d3e9e1df
Igor Knop <igorknop@gmail.com>
2023-09-09 00:09:01 -0300

    return index.html

--- index.tsx
+++ index.tsx
@@ -9,6 +9,9 @@ console.log(`Server running at http://${server.hostname}:${server.port}/`);
 
 function handler(request:Request):Response {
   const url = new URL(request.url);
+  if(url.pathname === '/' || url.pathname === '/index.html' || url.pathname === '') {
+    return new Response(Bun.file('./index.html'));
+  }
   return new Response(`Not Found: ${url}`, { status: 404 });
   
 }
